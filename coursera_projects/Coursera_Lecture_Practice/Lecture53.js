d987576aa3fc87c4b72013f1b295006919201162
L Barbee <bluelb84@gmail.com>
2017-02-20 14:57:24 -0600

    made changes to Lecture 53 practice code

--- coursera_projects/Coursera_Lecture_Practice/Lecture53.js
+++ coursera_projects/Coursera_Lecture_Practice/Lecture53.js
@@ -1,25 +1,37 @@
-function sayHello () {
-  var name =
-    document.getElementById("name").value;
-    var message = "<h2>Hello " + name + "!</h2>";
+document.addEventListener("DOMContentLoaded",
+  function (event) {
 
-    // document
-    //  .getElementById("content")
-    //  .textContent = message;
+    function sayHello (event) {
+      this.textContent = "Said it!"; //"this" is pointing to the button, so this line is changing the text content of the button when the sayHello function is executed, aka, the button is clicked
+      var name =
+        document.getElementById("name").value;
+        var message = "<h2>Hello " + name + "!</h2>";
 
-    document
-      .getElementById("content")
-      .innerHTML = message;
+        // document
+        //  .getElementById("content")
+        //  .textContent = message;
 
-    if (name === "student") {
-      var title =
         document
-          .querySelector("#title")
-          .textContent;
-      title += " & Lovin' it!";
-      document
-        //.querySelector("#title") calling by the selector like in CSS
-        .querySelector("h1") // will return the first h1 element
-        .textContent = title;
+          .getElementById("content")
+          .innerHTML = message;
+
+        if (name === "student") {
+          var title =
+            document
+              .querySelector("#title")
+              .textContent;
+          title += " & Lovin' it!";
+          document
+            //.querySelector("#title") calling by the selector like in CSS
+            .querySelector("h1") // will return the first h1 element
+            .textContent = title;
+        }
     }
-}
+    // Unobstrusive event binding Lecture 54
+    // document.querySelector("button")
+    //   .addEventListener("click", sayHello);
+
+    document.querySelector("button").onclick = sayHello; //setting the VALUE of sayHello
+    //whatever property of the element you have available, you can use it as a property of the object that you selected. For example document.querySelector("button").onclick
+  }
+);
